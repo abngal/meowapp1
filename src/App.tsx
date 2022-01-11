1f38ab2881ac9abd86ed86e644a25bbbe1116cab
abner <abgalenojr@gmail.com>
2022-01-11 17:27:17 +0800

    updates parent App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import axios from "axios";
+
+import CatDetail from './components/cat/CatDetail';
+import CatList from './components/cat/CatList';
 import './App.css';
 
-function App() {
+axios.defaults.headers.common['x-api-key'] = `ac9d1e26-8947-4d23-b8a4-d2586e445154`;
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'
+
+
+const App: React.FC = () => {
+
   return (
     <div className="App">
-      blank
+      
+        <BrowserRouter>
+          <Routes>
+            
+                <Route path="/" element={<CatList/>}></Route>
+                <Route path="/cat/:imageId" element={<CatDetail/>}></Route>
+          </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
