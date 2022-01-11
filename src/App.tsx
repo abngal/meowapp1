24c23240d6f6016d6fd9be5d6df1a733549b84cd
abngal <abgalenojr@gmail.com>
2022-01-11 12:41:40 +0000

    Redirect to home, on wrong path

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,22 @@ import axios from "axios";
 import CatDetail from './components/cat/CatDetail';
 import CatList from './components/cat/CatList';
 import './App.css';
+import NotFound from './components/common/NotFound';
 
 axios.defaults.headers.common['x-api-key'] = `ac9d1e26-8947-4d23-b8a4-d2586e445154`;
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'
-
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
-      
         <BrowserRouter>
           <Routes>
-            
-                <Route path="/" element={<CatList/>}></Route>
-                <Route path="/cat/:imageId" element={<CatDetail/>}></Route>
+              <Route path="/" element={<CatList/>} > </Route>
+              <Route path="/cat/:imageId" element={<CatDetail/>} > </Route>
+              <Route path="*" element={<NotFound/>} > </Route>
           </Routes>
         </BrowserRouter>
-
     </div>
   );
 }
---
+++ src/components/common/NotFound.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+
+    const navigate = useNavigate();
+    const timeBeforeRedirect = 5;
+
+    useEffect( () => {
+        setTimeout(() => {
+            navigate("/")
+        }, timeBeforeRedirect * 1000 );
+
+    }, []);
+
+    return (
+        <div>
+            <br/>
+            <br/>
+            <br/>
+            Will redirect to <Link to="/"> Homepage </Link>  in {timeBeforeRedirect}s.
+        </div>
+    )
+
+}
+
+export default NotFound;
